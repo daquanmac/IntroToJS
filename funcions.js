1e6c498dfa4126965972d4b372624cf1bb8506c7
Daquan Mac <Daquan@Daquans-MacBook-Air.local>
2017-07-26 12:07:45 -0500

    first commit

---
+++ funcions.js
@@ -0,0 +1,109 @@
+// 1.
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// Then, write and example of using the function.
+
+function max(num1, num2){
+  if (num1 > num2){
+    return num1;
+  } else {
+    return num2
+  }
+
+}
+
+
+// 2.
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// Then, write and example of using the function.
+
+function maxOfThree(num1, num2, num3){
+  var largestNum;
+if (num1 > num2 && num1 > num3) {
+  largestNum = num1;
+} else if (num2 > num1 && num2 > num3) {
+  largestNum = num2;
+} else if (num3 > num1 && num3 > num2) {
+  largestNum = num3;
+} else {
+  return "argument error";
+};
+return largestNum;
+}
+}
+
+
+// 3.
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// Then, write and example of using the function.
+
+function isVowel(char){
+  if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// 4.
+// Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
+// Then, write and example of using the function.
+function sum(num1, num2){
+  return num1 + num2
+}
+
+
+// 5.
+// Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
+// Then, write and example of using the function.
+function avg(num1, num2, num3){
+  return (num1 + num2 + num3) / 3
+}
+
+
+// 6.
+// Write a function called `getLength` that takes one parameter (a string) and returns the length
+// Then, write and example of using the function.
+function getLength(sentence){
+  return sentence.length
+}
+
+
+// 7.
+// Write a function called `greaterThan` that takes two parameters
+// and returns `true` if the second parameter is greater than the first.
+// Otherwise the function should return `false`.
+// Then, write and example of using the function.
+function greaterThan(num1, num2) {
+  if (num2 > num1){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// 8.
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+// Then, write and example of using the function.
+function greet(YourName){
+  return "Hello," + YourName + "!";
+}
+
+
+// 9.
+// Write a function called `madlib` that takes 4 or more parameters (words).
+// The function should insert the words into a pre-defined sentence.
+// Finally the function should return that sentence.
+// Note: When I say words and sentence I mean strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+// Then, write and example of using the function.
+function madlib(input1, input2, input3, input4){
+  var sentence = input1 + input2 + input3 + "jumps over the" + input4
+  return sentence
+}
+madlib(quick, brown, fox, fence)
